ef9a5349cac8fc30283dc5e59aa5cfc239dc550a
yungpanko <jrjohns@mit.edu>
2017-06-03 20:32:45 -0400

    Done.

--- application.rb
+++ application.rb
@@ -2,7 +2,7 @@ class Application
 
   def call(env)
     resp = Rack::Response.new
-    resp.write "Hello, World, my name is Jared"
+    resp.write "Hello, my name is Jared"
     resp.finish
   end
 
